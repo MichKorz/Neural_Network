dea5c792d34d1ba006f53b7665fc3740d975b843
M.Korzeniewski <43995863+MichKorz@users.noreply.github.com>
2024-05-29 13:30:12 +0200

    Added handling for custom data paths

--- main.py
+++ main.py
@@ -6,23 +6,22 @@ import sys
 
 
 if len(sys.argv) == 1:
-    network = Network("MNIST#7", False, 0.5, 784, [12], 10)
+    network = Network("MNIST#7", False, 0.1, 784, [12], 10)
     data_loader = DataLoader("MNIST/train_data.data", "MNIST/train_labels.data",
                              "MNIST/test_data.data", "MNIST/test_labels.data")
     coach = Coach(network, data_loader, True)
 
-    coach.train(5, 100)
+    coach.train(50, 100)
     sys.exit(0)
 
-ph = ParametersHandler(sys.argv)
+args = sys.argv[5:15]
+ph = ParametersHandler(args)
+
 network = Network(ph.name, ph.load, ph.learning_rate, ph.input_size, ph.h_layers, ph.output_size)
-data_loader = DataLoader("C:/Users/Admin/PycharmProjects/NeuralNetwork/MNIST/train_data.data",
-                         "C:/Users/Admin/PycharmProjects/NeuralNetwork/MNIST/train_labels.data",
-                         "C:/Users/Admin/PycharmProjects/NeuralNetwork/MNIST/test_data.data",
-                         "C:/Users/Admin/PycharmProjects/NeuralNetwork/MNIST/test_labels.data")
+data_loader = DataLoader(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
 coach = Coach(network, data_loader, ph.variable_learning_rate)
 
-print("Setup compleate, begining training!", flush=True)
+print("Setup complete, beginning training!", flush=True)
 
 coach.train(ph.epochs, ph.samples_per_epoch)
 sys.exit(0)
